9f29d16ec733ce38d2c3ce183265c7b60d6a4860
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-23 22:43:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,11 +7,7 @@ class Shoe
    def initialize(brand)
    @brand = brand 
    BRANDS << brand 
-   if "BRANDS" == brand 
-     puts "true"
-   else 
-     nil 
-   end 
+   
   end 
   
   def cobble
